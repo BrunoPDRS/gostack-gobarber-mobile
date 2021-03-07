743d3c5502c999136898107afa6815d7a7d09acd
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-03-06 21:57:05 -0300

    First commit, start gobarber app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <Text>Hello GoBarber</Text>
+  </View>
+);
+
+export default App;
