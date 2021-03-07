330dc782620b779a7033ab8383023cb3712b8a65
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-03-07 02:23:26 -0300

    Handling statusbar look

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Hello GoBarber</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38"/>
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Text>Hello GoBarber</Text>
+    </View>
+  </>
 );
 
 export default App;
