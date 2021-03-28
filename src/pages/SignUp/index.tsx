81f115831eda7f15eb9164a83de82c6824dd2cd5
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-03-27 22:58:46 -0300

    refactor sign up pages to integrate unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from 'react-native-vector-icons/Feather';
+import { Form } from '@unform/mobile';
+import {FormHandles} from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -15,8 +17,10 @@ import {
   BackToSignInText
 } from './styles';
 import { ScrollView } from 'react-native-gesture-handler';
+import { FormProvider } from '@unform/core';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -37,12 +41,13 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={() => {}}>
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button onPress={() => {console.log('oi')}}>Cadastrar</Button>
-
+            <Button onPress={() => {formRef.current?.submitForm()}}>Cadastrar</Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
